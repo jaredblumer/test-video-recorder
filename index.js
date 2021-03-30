6c0f6d8830fbcd73a52211eeb6bc208c7f1ce233
Jared Blumer <jaredgblumer@gmail.com>
2021-03-30 16:10:45 -0400

    Revert URI encoding RegEx to only replace spaces, not symbols

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let ffmpeg, logPath;
 
 // get current test title and clean it, to use it as file name
 function fileName(title) {
-  return encodeURIComponent(title.trim().replace(/["~!@#$%^&*\(\)_+=`{}\[\]\|\\:;'<>,.\/?"\- \t\r\n]+/g, '-'));
+  return encodeURIComponent(title.trim().replace(/\s+/g, "-"));
 }
 
 // build file path
